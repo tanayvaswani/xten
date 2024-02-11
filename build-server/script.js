c2a9487dae0637f341aa2b43a4aff1623b16d698
tanayvaswani <vaswani.tanay9@gmail.com>
2024-02-11 20:26:07 +0530

    feat: s3 client and everthing

--- build-server/script.js
+++ build-server/script.js
@@ -1,6 +1,16 @@
 const { exec } = require("child_process");
 const path = require("path");
 const fs = require("fs");
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const mime = require("mime-types");
+
+const s3Client = new S3Client({
+  region: "",
+  credentials: {
+    accessKeyId: "",
+    sessionToken: "",
+  },
+});
 
 async function init() {
   console.log(`Executing script.js`);
@@ -19,8 +29,30 @@ async function init() {
   });
 
   // On completion
-  p.on("close", () => {
+  p.on("close", async () => {
     console.log("Build Complete!");
     const distDirPath = path.join(__dirname, "output", "dist");
+
+    const distDirContent = fs.readdirSync(distDirPath, { recursive: true });
+
+    for (const filePath of distDirContent) {
+      if (fs.lstatSync(filePath).isDirectory()) {
+        continue;
+      }
+
+      // upload on S3
+      const command = PutObjectCommand({
+        // bucket name
+        Bucket: "",
+        Key: `__outputs/${process.env.PROJECT_ID}/${filePath}`,
+        Body: fs.createReadStream(filePath),
+        ContentType: mime.lookup(filePath),
+      });
+
+      // upload objects to s3 bucket
+      await s3Client.send(command);
+
+      console.log(`Process done...`);
+    }
   });
 }
